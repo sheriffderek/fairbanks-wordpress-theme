8a1aaf9295be744c1e6577b031099da5ce29e259
Derek Wood <derek@sheriffderek.consulting>
2019-09-02 18:20:38 -0700

    WP: Add in slider data

--- wp-content/themes/fairbanks/modules/landing-splash.php
+++ wp-content/themes/fairbanks/modules/landing-splash.php
@@ -19,3 +19,45 @@
 
 </div>
 </section>
+
+
+
+<section class="page-section landing-splash-slider">
+<div class="inner-column">
+
+  <h2>Slider data:</h2>
+
+  <ul class="slide-list">
+    <?php
+      $rows = get_field('slide');
+    ?>
+
+    <?php foreach($rows as $row) { ?>
+      <li>
+        <ul>
+          <li>
+            <?=$row['slide_heading']?>
+          </li>
+
+          <li>
+            <?=$row['slide_subheading']?>
+          </li>
+
+          <li>
+            <?=$row['slide_background_image']['url']?>
+          </li>
+
+          <li>
+            <?=$row['slide_link']?>
+          </li>
+
+          <li>
+            <?=$row['slide_button_text']?>
+          </li>
+        </ul>
+      </li>
+    <?php } ?>
+  </ul>
+
+</div>
+</section>
